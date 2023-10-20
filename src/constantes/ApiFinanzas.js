ebaf57a90655a4364c5414665f7a1542f58aa581
masacr3 <leorhcp2030@gmail.com>
2023-10-20 01:08:28 -0300

    preparando deploy

--- src/constantes/ApiFinanzas.js
+++ src/constantes/ApiFinanzas.js
@@ -1,4 +1,5 @@
-const _endPoint = 'http://192.168.0.8:8000/'
+//const _endPoint = 'http://192.168.0.8:8000/'
+const _endPoint = 'https://fastapitest-mjo6.onrender.com/'
 
 export const ApiSaldo = _endPoint +'finanzas/saldo'
 export const ApiGastos = _endPoint +'finanzas/gastos'
